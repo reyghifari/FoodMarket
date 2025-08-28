a212619b80b850ab06f0afd5ec7ed6a6a113a2d3
Kgs Muhammad Raihan Ghifari <raihan.ghifari1603@gmail.com>
2025-08-28 13:37:38 +0700

    Initial commit

---
+++ composeApp/src/commonMain/kotlin/com/raihan/market/Greeting.kt
@@ -0,0 +1,9 @@
+package com.raihan.market
+
+class Greeting {
+    private val platform = getPlatform()
+
+    fun greet(): String {
+        return "Hello, ${platform.name}!"
+    }
+}
---
+++ composeApp/src/commonTest/kotlin/com/raihan/market/ComposeAppCommonTest.kt
@@ -0,0 +1,12 @@
+package com.raihan.market
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ComposeAppCommonTest {
+
+    @Test
+    fun example() {
+        assertEquals(3, 1 + 2)
+    }
+}
